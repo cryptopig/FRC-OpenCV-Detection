277a448aec35506d6a5cbfbb3596e4aab99fe996
cryptopig <71409503+cryptopig@users.noreply.github.com>
2022-10-17 22:40:25 -0700

    Add video and image reading

--- mainScript.py
+++ mainScript.py
@@ -1,9 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv # this singular line took 3 days to fix
 
-# Import the image
-img = cv2.imread('burano.jpg')
-plt.imshow(img)
+# IMAGE READING __________________________
+# Points to photo of cone
+img = cv.imread('Photos/cones2.jpg')
+
+# Name and array size of pixels
+cv.imshow('Cone', img)
 
 
+
+
+# VIDEO READING __________________________
+
+# Points to location of video file
+capture = cv.VideoCapture('Videos/dog_shaving.mp4')
+
+# Reads video frame by frame
+while True:
+        isTrue, frame = capture.read()
+
+# Waits for hotkey, time in miliseconds
+cv.waitKey(0)
