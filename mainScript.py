37931c18000df27e5c9b1806acee2529328a90ab
cryptopig <71409503+cryptopig@users.noreply.github.com>
2022-10-17 22:46:30 -0700

    Update mainScript.py

--- mainScript.py
+++ mainScript.py
@@ -1,5 +1,6 @@
 import cv2 as cv # this singular line took 3 days to fix
 
+'''
 # IMAGE READING __________________________
 # Points to photo of cone
 img = cv.imread('Photos/cones2.jpg')
@@ -7,17 +8,24 @@ img = cv.imread('Photos/cones2.jpg')
 # Name and array size of pixels
 cv.imshow('Cone', img)
 
-
+# Waits for hotkey, time in miliseconds
+cv.waitKey(0)
+'''
 
 
 # VIDEO READING __________________________
 
 # Points to location of video file
-capture = cv.VideoCapture('Videos/dog_shaving.mp4')
+capture = cv.VideoCapture('Videos/dog_shaving.mp4') # Capture is an instance of the VideoCapture class
 
 # Reads video frame by frame
 while True:
-        isTrue, frame = capture.read()
+        isTrue, frame = capture.read() # uses capture.read() and cv.imshow() to display all frames
+        cv.imshow('Video', frame)
 
-# Waits for hotkey, time in miliseconds
-cv.waitKey(0)
+        if cv.waitKey(20) & 0xFF == ord('d'):
+                break
+
+capture.release()
+cv.destroyAllWindows()
+
