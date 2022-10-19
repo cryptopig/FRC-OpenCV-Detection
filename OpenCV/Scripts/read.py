5b1bd6155bb2521c41847583f7dbcccdceb0842b
cryptopig <71409503+cryptopig@users.noreply.github.com>
2022-10-19 16:07:16 -0700

    Create read.py

---
+++ OpenCV/Scripts/read.py
@@ -0,0 +1,28 @@
+import cv2 as cv # this singular line took 3 days to fix
+
+'''
+# IMAGE READING __________________________
+# Points to photo of cone
+img = cv.imread('Photos/cones2.jpg')
+# Name and array size of pixels
+cv.imshow('Cone', img)
+# Waits for hotkey, time in miliseconds
+cv.waitKey(0)
+'''
+
+
+# VIDEO READING __________________________
+
+# Points to location of video file
+capture = cv.VideoCapture('Videos/dog_shaving.mp4') # Capture is an instance of the VideoCapture class
+
+# Reads video frame by frame
+while True:
+        isTrue, frame = capture.read() # uses capture.read() and cv.imshow() to display all frames
+        cv.imshow('Video', frame)
+
+        if cv.waitKey(20) & 0xFF == ord('d'):
+                break
+
+capture.release()
+cv.destroyAllWindows()
