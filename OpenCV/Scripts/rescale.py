9d00c738ba6262a76d4e77853536814fc2b5a1d6
cryptopig <71409503+cryptopig@users.noreply.github.com>
2022-10-25 23:11:12 -0700

    added rescaling frames of videos

--- OpenCV/Scripts/rescale.py
+++ OpenCV/Scripts/rescale.py
@@ -1,9 +1,41 @@
-import cv2 as cv
+import cv2 as cv # this singular line took 3 days to fix
 
-img = cv.imread('Photos/cone1.jpg')
+
+# IMAGE READING __________________________
+
+# Points to photo of cone
+img = cv.imread('Photos/cones2.jpg')
+
+# Name and array size of pixels
 cv.imshow('Cone', img)
 
-def rescaleFrame(frame, scale):
-    
+# Rescaling: Takes a frame and scales it by a scale value
+def rescaleFrame(frame, scale = 0.75):
+    width = int(frame.shape[1] * scale) # frame.shape[1] is width
+    height = int(frame.shape[0] * scale) # frame.shape[0] is length
 
-cv.waitKey(0)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+
+# Reads video frame by frame
+capture = cv.VideoCapture('Videos/dog_shaving.mp4') # Capture is an instance of the VideoCapture class
+
+
+while True:
+        isTrue, frame = capture.read() # uses capture.read() and cv.imshow() to display all frames
+
+        frame_resized = rescaleFrame(frame)
+        cv.imshow('Video Resized', frame_resized)
+
+        cv.imshow('Video', frame)
+
+        if cv.waitKey(20) & 0xFF == ord('d'):
+                break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+
+
